ed77a521dbb7118ad5b3ed120500fef207763167
Zachary Wooten <zbwoote@clemson.edu>
2026-03-13 00:35:38 -0400

    Frontend now redirecting to spotify auth login  via backend
    Need to make useSpotifyAuth hook next

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Dashboard from "./pages/Dashboard";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
